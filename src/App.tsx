2ee6d32d382299b68010f687754ff97bb8075034
Marcio Junior <marcio.mj70@gmail.com>
2022-02-03 11:34:35 -0300

    feat: Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import SidebarWithHeader from './components/Sidebar';
+import Login from './pages/Login';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ SidebarWithHeader } />
+        <Route path="/login" component={ Login } />
       </Switch>
     </BrowserRouter>
   )
