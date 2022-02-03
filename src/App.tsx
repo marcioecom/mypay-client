7eea1462b328aa6e18a6051d534e19d48b3628a4
Marcio Junior <marcio.mj70@gmail.com>
2022-02-03 11:36:08 -0300

    feat: Register Page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import SidebarWithHeader from './components/Sidebar';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={ SidebarWithHeader } />
         <Route path="/login" component={ Login } />
+        <Route path="/register" component={ Register } />
       </Switch>
     </BrowserRouter>
   )
