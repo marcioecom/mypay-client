b9f62efd6c97bd990f8a0e10c0020f9c9a7c9a28
Marcio Junior <marcio.mj70@gmail.com>
2022-02-04 21:18:21 -0300

    feat: Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import SidebarWithHeader from './components/Sidebar';
+
+import Home from './pages/Home';
 import Login from './pages/Login';
 import Products from './pages/Products';
 import Profile from './pages/Profile';
@@ -10,7 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={ SidebarWithHeader } />
+        <Route exact path="/" component={ Home } />
         <Route path="/login" component={ Login } />
         <Route path="/register" component={ Register } />
         <Route path="/products" component={ Products } />
---
+++ src/pages/Home.tsx
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import {
+  Box,
+  Heading,
+  Container,
+  Text,
+  Button,
+  Stack,
+} from '@chakra-ui/react';
+import { FiArrowRight } from 'react-icons/fi';
+import NavBar from '../components/NavBar';
+import { useHistory } from 'react-router-dom';
+
+function Home() {
+  const history = useHistory();
+
+  return (
+    <>
+      <NavBar />
+      <Container maxW={'3xl'}>
+        <Stack
+          as={Box}
+          textAlign={'center'}
+          spacing={{ base: 8, md: 14 }}
+          py={{ base: 20, md: 36 }}>
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
+            lineHeight={'110%'}>
+            Make money from <br />
+            <Text as={'span'} color={'green.400'}>
+              your audience
+            </Text>
+          </Heading>
+          <Text color={'gray.500'}>
+            Monetize your content by charging your most loyal readers and reward
+            them loyalty points. Give back to your loyal readers by granting
+            them access to your pre-releases and sneak-peaks.
+          </Text>
+          <Stack
+            direction={'column'}
+            spacing={3}
+            align={'center'}
+            alignSelf={'center'}
+            position={'relative'}>
+            <Button
+              colorScheme={'purple'}
+              // bg={'green.400'}
+              color='white'
+              bg={'#5850ec'}
+              // rounded={'full'}
+              px={6}
+              rightIcon={<FiArrowRight />}
+              onClick={() => history.push('/products')}
+            >
+              Cadastrar
+            </Button>
+          </Stack>
+        </Stack>
+      </Container>
+    </>
+  )
+}
+
+export default Home;
