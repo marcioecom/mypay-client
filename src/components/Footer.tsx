187f17d31e95b824ec1e2dd775882fdceac301c7
Marcio Junior <marcio.mj70@gmail.com>
2022-02-07 00:18:13 -0300

    feat: Footer Component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,32 @@
+import {
+  Box,
+  Container,
+  Link as LinkStyle,
+  Stack,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+
+export default function Footer() {
+  return (
+    <Box
+      bg={useColorModeValue('gray.50', 'gray.900')}
+      color={useColorModeValue('gray.700', 'gray.200')}>
+      <Container
+        as={Stack}
+        maxW={'6xl'}
+        py={4}
+        direction={{ base: 'column', md: 'row' }}
+        spacing={4}
+        justify={{ base: 'center', md: 'space-between' }}
+        align={{ base: 'center', md: 'center' }}>
+        <Text>© 2020 Chakra Templates. All rights reserved</Text>
+
+        <Link to="/a">
+          <LinkStyle>Termos de uso</LinkStyle>
+        </Link>
+      </Container>
+    </Box>
+  );
+}
